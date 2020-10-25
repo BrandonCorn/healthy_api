04aef3f6f54df18403fa90c752cd2926a24f1aee
BrandonCorn <corn_brandon@columbusstate.edu>
2020-10-25 12:30:04 -0600

    calculations for BMI and maintenance calorie intake completed

--- controllers/calculations.js
+++ controllers/calculations.js
@@ -8,26 +8,45 @@ const inToCm = inches => inches * 2.54
 const lbToKg = pounds => pounds * .454
 
 
+//BMI Formula
+//** BMI = (wLb / hIn**2) * 703 */
+//** RETURN VALUE NEEDS TO BE ROUNDED TWO PLACES */
+const calcBMI = personData => (personData.weight / personData.height**2) * 703
+
+
 //BMR formula
-//** Male: 66.4730 + (13.7516 * wKg) + (5.0033 * hCm) - (6.7550 * ageY)
-//** Female: 655.0955 + (9.5634 * wKg) + (1.8496 * hCm) - (4.6756 * ageY) */ */
+//** Male: 66.4730 + (6.3 * wLb) + (12.9 * hIn) - (6.8 * ageY)
+//** Female: 655 + (4.3 * wLb) + (4.7 * hIn) - (4.7 * ageY) */ */
 //** BMR NEEDS TO BE ROUNDED TWO PLACES!! */
-const calcBmr = personData => {
+const calcBMR = personData => {
     if (personData.gender === 'male'){
-        let bmr = 66.4730 + (13.7516 + personData.weight) + (5.0033 * personData.height)
-            - (6.7550 * personData.age); 
-        console.log('male_bmr: ',bmr); 
-        return [null, bmr]; 
+        let bmr = 66 + (6.3 * personData.weight) + (12.9 * personData.height) 
+            - (6.8 * personData.age); 
+        return bmr; 
     }
     else if (personData.gender === 'female'){
-        let bmr = 655.0955 + (9.5634 * personData.weight) + (1.8496 * personData.height)
-            - (4.6756 * personData.age); 
-        console.log('female_bmr: ',bmr); 
-        return [null, bmr]; 
+        let bmr = 655 + (4.3 * personData.weight) + (4.7 * personData.height)
+            - (4.7 * personData.age);  
+        return bmr; 
     }
     else {
-        return ['User has no gender', null]
+        return; 
     }
+}
+
+// To calculate calorie intake we use the activity level of person on scale from 1 - 5
+// This returns calories to maintain current weight
+// 1: Little or no exercise, intake = BMR * 1.2
+// 2: Light exercise/sports 1-3 days a week, intake = BMR * 1.375
+// 3: Moderate exercise/sports 3-5 days a week, intake = BMR * 1.55
+// 4: Hard exercise/sports 6-7 days a week, intake = BMR * 1.725
+// 5: Very Hard exercise/sports & physical job or 2 a day training, intake = BMR * 1.9
+const calcCalories = (activity,bmr) => {
+    if (activity == 1 ) return bmr * 1.2
+    else if (activity == 2) return bmr * 1.375
+    else if (activity == 3) return bmr * 1.55
+    else if (activity == 4) return bmr * 1.725
+    else if (activity == 5) return bmr * 1.9
 }
 
 
@@ -35,5 +54,7 @@ const calcBmr = personData => {
 module.exports = {
     inToCm, 
     lbToKg,
-    calcBmr,
+    calcBMI,
+    calcBMR,
+    calcCalories,
 }
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,30 @@
 const router = require('express').Router(); 
-const { inToCm, lbToKg, calcBmr,} = require('../controllers/calculations'); 
-// const validate = require('../controllers/validate');
+const { inToCm, lbToKg, calcBMI, calcBMR, calcCalories } = require('../controllers/calculations'); 
 const validate = require('../middleware/validate');  
 
-router.get('/calc_health_stats', validate, (req,res) => { 
-    // const validData = validate(req.query); 
-    // console.log('valid: ', validData); 
-
-    const info = {
-        height: inToCm(req.query.height), 
-        weight: lbToKg(req.query.weight), 
+router.get('/calc_health_stats', validate, async (req,res) => { 
+    const userData = {
+        height: req.query.height, 
+        weight: req.query.weight, 
         age: req.query.age, 
         gender: req.query.gender.toLowerCase().trim(), 
         activity: req.query.activity.trim(),
     } 
-    console.log(info); 
-    const [err, bmr] = calcBmr(info); 
-    res.send(info); 
+
+    const healthData = {
+        BMI: calcBMI(userData), 
+        BMR: calcBMR(userData), 
+    }
+
+    healthData.Calories = await calcCalories(userData.activity,healthData.BMR); 
+    console.log('bmi: ',healthData.BMI); 
+    console.log('bmr: ', healthData.BMR); 
+    console.log('calories: ', healthData.Calories); 
+    if (!healthData.BMI) res.status(400).send('Error calculating BMI'); 
+    if (!healthData.BMR) res.status(400).send('Error calculating BMR'); 
+    if (!healthData.Calories) res.status(400).send('Error calculating maintenance calorie intake'); 
+
+    res.status(200).send(healthData); 
 })
 
 
